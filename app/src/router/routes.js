2f2042f5538ed2f757e5f3bbe1083c65120113dc
Brian Winkers <bwinkers@gmail.com>
2023-11-30 11:29:50 -0800

    Policy routes

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -4,7 +4,15 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', name: "home", component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+
+  {
+    path: '/legal/:policy',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', name: "policyPage", component: () => import('pages/PolicyPage.vue') }
     ]
   },
 
